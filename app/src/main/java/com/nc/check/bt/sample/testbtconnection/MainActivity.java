a1b2201123a56f790b7bb31163e91bdf7a831e9f
iee <iee@FUP-423>
2018-08-23 15:57:11 +0300

    add chat

--- app/src/main/java/com/nc/check/bt/sample/testbtconnection/MainActivity.java
+++ app/src/main/java/com/nc/check/bt/sample/testbtconnection/MainActivity.java
@@ -5,5 +5,22 @@ import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends BaseBluetoothActivity {
+    public void start(View view){
+        btController.start();
+    }
+    public void connect(View view){
+        btController.connect();
+    }
+    public void stop(View view){
+        btController.stop();
+    }
 
+    @Override
+    protected void writeReceivedMessage(String strIncom) {
+        ((TextView)findViewById(R.id.label_data)).setText(strIncom);
+    }
+    public void send(View view){
+        String message = ((TextView)findViewById(R.id.et_cmd)).getText().toString();
+        btController.write(message);
+    }
 }
